d726cd756446cfc92b2da814dd6fac5a28d97d85
Eshaan Issar <eshaanissar@outlook.com>
2025-09-13 23:22:45 -0400

    add vale-lsp

--- lua/lazy-plugins/lsp.lua
+++ lua/lazy-plugins/lsp.lua
@@ -158,6 +158,10 @@ return {
             --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
             local servers = {
                 marksman = {},
+                -- local vale_ls = require("lspconfig.configs.vale_ls")
+                vale_ls = {
+                    filetypes = { 'markdown', 'text', 'tex', 'rst' },
+                },
                 gopls = {
                     settings = {
                         gofumpt = true,
