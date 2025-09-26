59e5715d4b7e6120d3af3ef3c450a2fa1b71c061
Eshaan Issar <eshaanissar@outlook.com>
2025-09-26 15:22:40 -0400

    enable exrc

--- lua/settings.lua
+++ lua/settings.lua
@@ -93,4 +93,7 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 2
+
+vim.opt.exrc = true
+vim.opt.secure = true
